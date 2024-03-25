7f07cdfacebc666e74ea1ddd9ebe56ee42ccb898
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-03-25 18:16:37 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2);
+}
+
+console.log(returnFirstTwoDrivers());
+function returnLastTwoDrivers(){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+  ];
+  
+  const getDriver = function () {
+    return selectingDrivers[0]();
+  }
+console.log(getDriver())
+function createFareMultiplier(fareMultiplier) {
+    return function(fare) {
+        return fare * fareMultiplier;
+    };
+  
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const fareQuintupler = createFareMultiplier(5);
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers))
+function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers);
+}
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers))
